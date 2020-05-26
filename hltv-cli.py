c93a8a4e2db9c29e55e5cc6794fccbfa2a1cb205
Gian <uni@esteves.net>
2020-05-26 14:03:04 -0400

    completed results menu

--- hltv-cli.py
+++ hltv-cli.py
@@ -26,7 +26,7 @@ def table30():
     list = hltv.top30()
     choice = input("Choose the rank of the team you want to view: ")
 
-    if(choice == 0):
+    if(int(choice) == 0):
         menu()
     else:
         choice = int(choice) - 1
@@ -38,22 +38,48 @@ def table30():
         for i in range(5):
             print(players[i].get('name'))
 
+def results():
+
+    titles = ["Teams", "Result", "Map", "Event"]
+
+    print("RESULTS")
+    print("-------------------------------------------------------------------------------------------------------------")
+    print("|" + titles[0].center(40) + "|" + titles[1].center(9) + "|" + titles[2].center(7) + "|" + titles[3].center(48) + "|")
+    print("-------------------------------------------------------------------------------------------------------------")
+
+    list = hltv.listresults()
+
+    for i in range(30):
+        teams = str(list[i].get('team1')) + " vs " + str(list[i].get('team2'))
+        result = str(list[i].get('score1')) + " - " + str(list[i].get('score2'))
+        map = str(list[i].get('map'))
+        event = str(list[i].get('event'))
+
+        print("|" + teams.center(40) + "|" + result.center(9) + "|" + map.center(7) + "|" + event.center(48) + "|")
+
+    print("-------------------------------------------------------------------------------------------------------------")
+
+
 def menu():
 
     print("Welcome to HLTV-CLI.py")
     print("Select a menu option")
+    print("0 - Return")
     print("1 - Top 30 Teams")
     print("2 - Results")
 
-    x = input("")
+    x = input()
 
-    pick(x)
+    pick(int(x))
 
 def pick(choice): 
-    switcher = { 
-        1: table30(), 
-        2: "one", 
-        3: "two", 
-    } 
+    if choice == 0:
+        menu()
+    elif choice == 1:
+        table30()
+    elif choice == 2:
+        results()
+    else:
+        print("Try again")
 
 menu()
